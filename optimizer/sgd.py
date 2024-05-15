c32583c7f25ba27ff24ef89c14e89f475207207a
Pouria_EsH <poriaes2002@gmail.com>
2024-05-15 21:33:40 +0330

    complete SGD

--- optimizer/sgd.py
+++ optimizer/sgd.py
@@ -3,10 +3,13 @@ from mytorch.layer import Layer
 from mytorch.optimizer import Optimizer
 
 class SGD(Optimizer):
-    def __init__(self, layers:List[Layer], learning_rate=0.1):
+    def __init__(self, layers: List[Layer], learning_rate=0.1):
         super().__init__(layers)
         self.learning_rate = learning_rate
 
     def step(self):
         "TODO: implement SGD algorithm"
-        pass
+        for l in self.layers:
+            l.weight = l.weight - l.weight.grad * self.learning_rate
+            if l.need_bias:
+                l.bias = l.bias - l.bias.grad * self.learning_rate
